d66dd849451e45c56edf05e3b23ea5a58abb8c5a
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-08-16 19:22:14 +0300

    21.commint

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace TraversalCoreProje.Controllers
@@ -13,6 +14,16 @@ namespace TraversalCoreProje.Controllers
             var values = destinationManager.TGetList();
 
             return View(values);
+        }
+        [HttpGet]
+        public IActionResult DestinationDetails(int id)
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult DestinationDetails(Destination p)
+        {
+            return View();
         }
     }
 }
