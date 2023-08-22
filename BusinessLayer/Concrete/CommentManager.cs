7881388520fab4be726fabddad2aba54bfa951c4
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-08-22 14:56:49 +0300

    55

--- BusinessLayer/Concrete/CommentManager.cs
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -25,17 +25,19 @@ namespace BusinessLayer.Concrete
 
         public void TDelete(Comment t)
         {
-            throw new NotImplementedException();
+            _commentDal.Delete(t);
+
         }
 
         public Comment TGetByID(int id)
         {
-            throw new NotImplementedException();
+            return _commentDal.GetById(id);
         }
+
 
         public List<Comment> TGetList()
         {
-            throw new NotImplementedException();
+            return _commentDal.GetList();
 
         }
 
@@ -47,6 +49,11 @@ namespace BusinessLayer.Concrete
         public void TUpdate(Comment t)
         {
             throw new NotImplementedException();
+        }
+
+        public List<Comment> TGetlistCommentWithDestination()
+        {
+            return _commentDal.GetlistCommentWithDestination();
         }
     }
 }
