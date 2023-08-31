0ba8bf342ea241ca51d22c98c70e36a862eeef42
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-08-31 17:26:18 +0300

    84

--- SignalRApi/Program.cs
+++ SignalRApi/Program.cs
@@ -1,16 +1,24 @@
 using Microsoft.EntityFrameworkCore;
 using SignalRApi.Dal;
+using SignalRApi.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+
+builder.Services.AddScoped<VisitorService>();
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
