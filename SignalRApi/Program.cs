5a514ea2fd839db608614c03c54dbee8744c2c62
nurullah namal <77736724+nurullahnamal@users.noreply.github.com>
2023-08-31 15:48:55 +0300

    83.postgresql

---
+++ SignalRApi/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using SignalRApi.Dal;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddEntityFrameworkNpgsql().AddDbContext<Context>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+//https://dev.to/moe23/net-6-with-postgresql-576a //postgresql .net 6
